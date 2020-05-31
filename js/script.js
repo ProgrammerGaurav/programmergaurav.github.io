bac728a36c5fce628369762c8351d5a0c2c1f269
ProgrammerGaurav <gauravgupta14376@gmail.com>
2020-05-31 13:36:20 +0530

    Hello World!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
+new WOW().init();
+
+$(document).ready(function () {
+    $(".se-pre-con").fadeOut("slow");
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 50) {
+            $(".navbar").css("background-color", "#111111");
+        } else {
+            $(".navbar").css("background-color", "transparent");
+        }
+    });
+    $('.navbar-collapse a').click(function () {
+        $(".navbar-collapse").collapse('hide');
+    });
+});
+
+var typed = new Typed("#typed", {
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    backSpeed: 10,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+$('.work').owlCarousel({
     items: 1,
     loop: true,
     autoplay: true,
@@ -7,15 +38,8 @@ owl.owlCarousel({
     center: true,
     autoplayTimeout: 3000,
     responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1,
-        }
-    }
 });
-var owl = $('.owl-carousels');
-owl.owlCarousel({
-    items: 1,
+$('.skill').owlCarousel({
     loop: true,
     autoplay: true,
     mouseDrag: true,
@@ -31,9 +55,7 @@ owl.owlCarousel({
         }
     }
 });
-var owl = $('.owl-carousels2');
-owl.owlCarousel({
-    items: 1,
+$('.certificate').owlCarousel({
     loop: true,
     autoplay: true,
     mouseDrag: true,
@@ -45,34 +67,11 @@ owl.owlCarousel({
         0: {
             items: 1,
         },
+        600: {
+            items: 2,
+        },
         1000: {
             items: 3,
         }
     }
-});
-new WOW().init();
-$(document).ready(function () {
-    $(".se-pre-con").fadeOut("slow");
-    $('.toggle').click(function () {
-        $('nav').toggleClass('active');
-    });
-    $(window).scroll(function () { // check if scroll event happened
-        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-            $(".navbars").css("background-color", "#25499F"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-        } else {
-            $(".navbars").css("background-color", "transparent"); // if not, change it back to transparent
-        }
-    });
-    $('.toggle').click(function () {
-        $('nav').toggleClass('menu')
-    });
-    $('.navopt').click(function () {
-        $('nav').toggleClass('menu')
-    });
-});
-var typed = new Typed("#typed", {
-    stringsElement: '#typed-strings',
-    typeSpeed: 30,
-    backSpeed: 10,
-    loop: true
 });
